f89ae856f2dde3333980fb0abedfc3952be08255
gravvivty <gayer.steven@gmail.com>
2025-04-06 15:18:15 +0200

    GlobaL dRAW

--- SWEN_Game/SWEN_Game/Renderer.cs
+++ SWEN_Game/SWEN_Game/Renderer.cs
@@ -126,5 +126,12 @@ namespace SWEN_Game
                 Matrix.CreateTranslation(screenCenter.X, screenCenter.Y, 0);
         }
 
+        // Draw a tile with its depth computed from its world position
+        private void DrawTile(SpriteBatch spriteBatch, Texture2D texture, Rectangle sourceRect, Vector2 position, LayerInstance layer)
+        {
+            float depth = _spriteManager.GetDepth(position, sourceRect.Height, layer);
+            Globals.SpriteBatch.Draw(texture, position, sourceRect, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, depth);
+        }
+
     }
 }
