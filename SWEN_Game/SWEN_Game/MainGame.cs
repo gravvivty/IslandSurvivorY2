06a50960a3ac1f20f995f688919f73201011b1e4
gravvivty <gayer.steven@gmail.com>
2025-04-06 14:31:59 +0200

    test4

--- SWEN_Game/SWEN_Game/MainGame.cs
+++ SWEN_Game/SWEN_Game/MainGame.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Assimp.Configs;
+﻿using System.Collections.Generic;
 using LDtk;
 using LDtkTypes;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using static LDtkTypes.Worlds;
 
 namespace SWEN_Game
 {
@@ -16,15 +11,17 @@ namespace SWEN_Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private GameManager _gameManager;
 
         public MainGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
 
             _graphics.PreferredBackBufferWidth = 1920;
             _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.HardwareModeSwitch = false;
             _graphics.IsFullScreen = false;
             _graphics.ApplyChanges();
         }
@@ -40,6 +37,14 @@ namespace SWEN_Game
         {
             // Create neccessary classes and set Global Values
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Globals.SpriteBatch = _spriteBatch;
+            Globals.Content = Content;
+            Globals.File = LDtkFile.FromFile("World", Content);
+            Globals.World = Globals.File.LoadWorld(Worlds.World.Iid);
+            Globals.Collisions = new List<Rectangle>();
+            Globals.Graphics = _graphics;
+            Globals.WindowSize = new Point(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            _gameManager = new GameManager();
 
             // TODO: use this.Content to load your game content here
         }
@@ -52,15 +57,19 @@ namespace SWEN_Game
             }
 
             // TODO: Add your update logic here
+            // Update
+            _gameManager.Update();
+            Globals.UpdateTime(gameTime);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _gameManager.Draw();
 
             base.Draw(gameTime);
         }
---
+++ SWEN_Game/SWEN_Game/_Managers/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SWEN_Game
+{
+    public class GameManager
+    {
+        private readonly Player _player;
+        //private readonly Renderer _renderer;
+        private readonly SpriteManager _spriteManager;
+        //private readonly SpriteCalculator _spriteCalculator;
+        //private readonly Debug _debug;
+
+        public GameManager()
+        {
+            _spriteManager = new SpriteManager();
+            _player = new Player(_spriteManager);
+            //_spriteCalculator = new SpriteCalculator(_spriteManager, _player);
+            //_renderer = new Renderer(_player, _spriteManager, _spriteCalculator);
+            //_debug = new Debug(_player, _renderer);
+
+            // Calculates ALL collisions in the level
+            Globals.CalculateAllCollisions();
+        }
+
+        public void Update()
+        {
+            // Every Frame check input
+            InputManager.Update(_player);
+            _player.Update();
+        }
+
+        public void Draw()
+        {/*
+            // Begin the sprite batch with depth sorting (FrontToBack) and apply the camera transformation.
+            Globals.SpriteBatch.Begin(
+                SpriteSortMode.FrontToBack,
+                transformMatrix: _renderer.CalcTranslation(),
+                samplerState: SamplerState.PointClamp);
+            _renderer.DrawWorld();
+            Globals.SpriteBatch.End();
+            _debug.DrawWorldDebug();
+
+            InputManager.DrawCursor();
+       */ }
+    }
+}
