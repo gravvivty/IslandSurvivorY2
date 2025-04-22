0170089b0377bcd858bb5461cd110f2427138cd8
gravvivty <gayer.steven@gmail.com>
2025-04-22 21:10:11 +0200

    Umstieg auf xUnit für createReports

--- SWEN_Game/SWEN_GameTests/ManagersTest/InputManagerTest.cs
+++ SWEN_Game/SWEN_GameTests/ManagersTest/InputManagerTest.cs
@@ -1,65 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
 using SWEN_Game;
+using Xunit;
 
 namespace SWEN_GameTests.ManagersTest
 {
-    [TestClass]
     public class InputManagerTest
     {
-        [TestMethod]
-
+        [Fact]
         public void Update_WKeyPressed_PlayerMovesUp()
         {
             // Arrange
             var player = new Player();
             Globals.Time = 1f;
-            // Set a fixed delta time for testing
+
+            // Act
             InputManager.Update(player, new KeyboardState(Keys.W));
 
             // Assert
-            Assert.AreEqual(-20, player.Position.Y);
-            Assert.AreEqual(100, player.Position.X);
+            Assert.Equal(-20, player.Position.Y);
+            Assert.Equal(100, player.Position.X);
         }
-        [TestMethod]
+
+        [Fact]
         public void Update_SKeyPressed_PlayerMovesDown()
         {
             // Arrange
             var player = new Player();
             Globals.Time = 1f;
 
+            // Act
             InputManager.Update(player, new KeyboardState(Keys.S));
+
             // Assert
-            Assert.AreEqual(220, player.Position.Y);
-            Assert.AreEqual(100, player.Position.X);
+            Assert.Equal(220, player.Position.Y);
+            Assert.Equal(100, player.Position.X);
         }
-        [TestMethod]
+
+        [Fact]
         public void Update_AKeyPressed_PlayerMovesLeft()
         {
             // Arrange
             var player = new Player();
             Globals.Time = 1f;
+
+            // Act
             InputManager.Update(player, new KeyboardState(Keys.A));
+
             // Assert
-            Assert.AreEqual(100, player.Position.Y);
-            Assert.AreEqual(-20, player.Position.X);
+            Assert.Equal(100, player.Position.Y);
+            Assert.Equal(-20, player.Position.X);
         }
 
-        [TestMethod]
+        [Fact]
         public void Update_DKeyPressed_PlayerMovesRight()
         {
             // Arrange
             var player = new Player();
             Globals.Time = 1f;
+
+            // Act
             InputManager.Update(player, new KeyboardState(Keys.D));
+
             // Assert
-            Assert.AreEqual(100, player.Position.Y);
-            Assert.AreEqual(220, player.Position.X);
+            Assert.Equal(100, player.Position.Y);
+            Assert.Equal(220, player.Position.X);
         }
     }
 }
-
