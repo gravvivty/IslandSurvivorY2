2b8a1aa3d243032fca483fc29d4458fa7bb7c570
gravvivty <gayer.steven@gmail.com>
2025-04-22 20:44:30 +0200

    Removed 2nd DrawTile for Pipeline Testing

--- SWEN_Game/SWEN_Game/_Graphics/Renderer.cs
+++ SWEN_Game/SWEN_Game/_Graphics/Renderer.cs
@@ -134,10 +134,6 @@ namespace SWEN_Game
         }
 
         // This causes the Pipeline to fail idk why even - it doesnt even get called as of now
-        private void DrawTile(SpriteBatch spriteBatch, Texture2D texture, Rectangle sourceRect, Vector2 position, float anchorDepth, LayerInstance layer)
-        {
-            float depth = _spriteManager.GetDepth(anchorDepth, layer);
-            spriteBatch.Draw(texture, position, sourceRect, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, depth);
-        }
+
     }
 }
