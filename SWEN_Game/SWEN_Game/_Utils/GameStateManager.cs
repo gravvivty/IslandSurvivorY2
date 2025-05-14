0dcc3dbffa17f185a172a61f6556b56c616c1025
Tatiana_malkina <malkinauf@gmail.com>
2025-05-14 09:35:22 +0200

    added GameState Manager

---
+++ SWEN_Game/SWEN_Game/_Utils/GameStateManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public enum GameState
+{
+    MainMenu,
+    Options,
+    Playing,
+    Paused,
+    GameOver
+}
+
+public static class GameStateManager
+{
+    public static GameState CurrentGameState { get; private set; } = GameState.MainMenu;
+
+    public static void SwitchGameState(GameState newGameState)
+    {
+        CurrentGameState = newGameState;
+        Console.WriteLine($"Switching from {CurrentGameState} to: {newGameState}");
+        CurrentGameState = newGameState;
+    }
+}
