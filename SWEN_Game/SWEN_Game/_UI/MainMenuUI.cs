1b17b21c1530c8611bbc971d86dc21b4d9cbf2ab
Tatiana_malkina <malkinauf@gmail.com>
2025-05-14 21:54:35 +0200

    add Button Options(non-functional), Exit

--- SWEN_Game/SWEN_Game/_UI/MainMenuUI.cs
+++ SWEN_Game/SWEN_Game/_UI/MainMenuUI.cs
@@ -14,18 +14,33 @@ public class MainMenuUI
         this.ui = uiSystem;
 
         // Create the root panel that contains all menu elements
-        rootPanel = new Panel(Anchor.Center, new Vector2(300, 200), Vector2.Zero);
+        rootPanel = new Panel(Anchor.Center, new Vector2(300, 300), Vector2.Zero, false, true);
         rootPanel.Texture = null;
         uiSystem.Add("MainMenu", rootPanel);
 
         // START Button: Switch game state to Playing
-        var startButton = new Button(Anchor.TopCenter, new Vector2(500, 100), "Start");
+        var startButton = new Button(Anchor.AutoCenter, new Vector2(500, 100), "Start");
         startButton.OnPressed += _ =>
         {
             System.Diagnostics.Debug.WriteLine("Start Clicked");
             GameStateManager.ChangeGameState(GameState.Playing);
         };
         rootPanel.AddChild(startButton);
+
+        var optionsButton = new Button(Anchor.AutoCenter, new Vector2(500, 100), "Options");
+        startButton.OnPressed += _ =>
+        {
+            System.Diagnostics.Debug.WriteLine("Options Clicked");
+        };
+        rootPanel.AddChild(optionsButton);
+
+        var exitButton = new Button(Anchor.AutoCenter, new Vector2(500, 100), "Exit");
+        exitButton.OnPressed += _ =>
+        {
+            System.Diagnostics.Debug.WriteLine("Exit Clicked");
+            uiSystem.Game.Exit();
+        };
+        rootPanel.AddChild(exitButton);
     }
 
     public void Show() => rootPanel.IsHidden = false;
