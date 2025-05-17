ded60f2ac642945d61b3047d655c04eddbd8129e
Esmo Morina <m2001esma@gmail.com>
2025-05-17 21:02:57 +0200

    Test für SpriteManager, Animation und Player

---
+++ SWEN_Game/SWEN_GameTests/ManagersTest/SpriteManagerTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using LDtk;
+using SWEN_Game;
+using Xunit;
+
+namespace SWEN_GameTests
+{
+    public class SpriteManagerTest
+    {
+        // Test für die Depth-Berechnung mit Position
+        [Fact]
+        public void GetDepth_WithPosition_ReturnsCorrectValue()
+        {
+            var manager = new SpriteManager();
+            var position = new Vector2(100, 200);
+            float spriteHeight = 32f;
+            float result = manager.GetDepth(position, spriteHeight);
+            Assert.Equal(0.232f, result); // (200 + 32) / 1000 = 0.232
+        }
+
+        // Test für Background Layer
+        [Fact]
+        public void GetDepth_WithBackgroundLayer_ReturnsInputValue()
+        {
+            var manager = new SpriteManager();
+            var layer = new LayerInstance { _Identifier = "Background" };
+            float inputDepth = 0.5f;
+            float result = manager.GetDepth(inputDepth, layer);
+            Assert.Equal(inputDepth, result); 
+        }
+        // Test für normale Layer
+        [Fact]
+        public void GetDepth_WithNormalLayer_ReturnsAdjustedValue()
+        {
+            var manager = new SpriteManager();
+            var layer = new LayerInstance { _Identifier = "Deco_Big1" };
+            float result = manager.GetDepth(0.5f, layer);
+            Assert.Equal(0.496f, result); // 0.5 - (4/1000) = 0.496
+        }
+    }
+}
