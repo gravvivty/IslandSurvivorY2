e950f1603100120e165299bdeb80d711269d7cd4
gravvivty <gayer.steven@gmail.com>
2025-05-29 18:20:01 +0200

    Added all weapons

--- SWEN_Game/SWEN_Game/_Shooting/WeaponManager.cs
+++ SWEN_Game/SWEN_Game/_Shooting/WeaponManager.cs
@@ -36,17 +36,32 @@ namespace SWEN_Game._Shooting
 
             Texture2D assaultRifleIconTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_UI/assault_rifle");
             Texture2D assaultIngameTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_Ingame/assault_rifle");
-            Weapon assault_rifle = new Weapon(0.15f, 300f, 0.7f, 1, 1, 4f, 24, 1.5f, 0, vanillaBulletTexture, assaultRifleIconTexture, assaultIngameTexture);
+            Weapon assault_rifle = new Weapon(0.15f, 300f, 0.75f, 1, 1, 4f, 24, 1.5f, 0, vanillaBulletTexture, assaultRifleIconTexture, assaultIngameTexture);
             _weapons.Add("Assault_Rifle", assault_rifle);
 
             Texture2D precisionRifleIconTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_UI/precision_rifle");
             Texture2D precisionRifleIngameTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_Ingame/precision_rifle");
-            Weapon precision_rifle = new Weapon(0.4f, 500f, 0.7f, 1, 1, 25f, 4, 2f, 3, vanillaBulletTexture, precisionRifleIconTexture, precisionRifleIconTexture);
+            Weapon precision_rifle = new Weapon(0.4f, 500f, 0.75f, 1, 1, 25f, 4, 2f, 3, vanillaBulletTexture, precisionRifleIconTexture, precisionRifleIconTexture);
             _weapons.Add("Precision_Rifle", precision_rifle);
 
+            Texture2D blunderbussIconTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_UI/blunderbuss");
+            Texture2D blunderbussIngameTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_Ingame/blunderbuss");
+            Weapon blunderbuss = new Weapon(0.5f, 300f, 1.25f, 1, 1, 25f, 1, 2.5f, 0, vanillaBulletTexture, blunderbussIconTexture, blunderbussIngameTexture);
+            _weapons.Add("Blunderbuss", blunderbuss);
+
+            Texture2D burstRifleIconTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_UI/burst_rifle");
+            Texture2D burstRifleIngameTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_Ingame/burst_rifle");
+            Weapon burst_rifle = new Weapon(0.5f, 300f, 1f, 1, 1, 6f, 21, 1.5f, 0, vanillaBulletTexture, burstRifleIconTexture, burstRifleIngameTexture);
+            _weapons.Add("Burst_Rifle", burst_rifle);
+
+            Texture2D revolverIconTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_UI/revolver");
+            Texture2D revolverIngameTexture = Globals.Content.Load<Texture2D>("Sprites/Guns/Guns_Ingame/revolver");
+            Weapon revolver = new Weapon(0.15f, 300f, 0.75f, 1, 1, 12.5f, 6, 1.5f, 1, vanillaBulletTexture, revolverIconTexture, revolverIngameTexture);
+            _weapons.Add("Revolver", revolver);
+
             // Get it twice cuz if not they have the same reference
-            PlayerGameData.Instance.BaseWeapon = this.GetWeapon("Pistol");
-            PlayerGameData.Instance.CurrentWeapon = this.GetWeapon("Pistol");
+            PlayerGameData.Instance.BaseWeapon = this.GetWeapon("Revolver");
+            PlayerGameData.Instance.CurrentWeapon = this.GetWeapon("Revolver");
             PlayerGameData.Instance.UpdateWeaponGameData();
         }
 
